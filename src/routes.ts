6292cec322569f018354da3c459dd8ea56bfa3ce
Luiz Gustavo <gusstavopdm@gmail.com>
2022-06-21 08:52:44 -0300

    feat(routes): adicionando route /authenticate

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
+import { AuthenticateClientController } from './modules/account/authenticateUser/AuthenticateClientController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
 import { Router } from 'express';
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
+const authenticateClientUser = new AuthenticateClientController();
 
 routes.post('/client/', createClientController.handle);
+routes.post('/authenticate', authenticateClientUser.handle);
 
 export { routes };
