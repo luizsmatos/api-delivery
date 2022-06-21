c0d2fe4de61b56cec901cd722f53331783cc5714
Luiz Gustavo <gusstavopdm@gmail.com>
2022-06-21 08:51:45 -0300

    feat(createClientController): removendo try/catch

--- src/modules/clients/useCases/createClient/CreateClientController.ts
+++ src/modules/clients/useCases/createClient/CreateClientController.ts
@@ -1,4 +1,3 @@
-import { Prisma } from '@prisma/client';
 import { Request, Response } from 'express';
 import { CreateClientUseCase } from './CreateClientUseCase';
 
@@ -6,23 +5,13 @@ export class CreateClientController {
   async handle(request: Request, response: Response) {
     const { username, password } = request.body;
 
-    try {
-      const createClientUseCase = new CreateClientUseCase();
+    const createClientUseCase = new CreateClientUseCase();
 
-      const client = await createClientUseCase.execute({
-        username,
-        password,
-      });
+    const client = await createClientUseCase.execute({
+      username,
+      password,
+    });
 
-      return response.json(client);
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        response.status(400).json({ error: error.message });
-      }
-
-      if (error instanceof Prisma.PrismaClientKnownRequestError) {
-        return response.status(400).json({ error: error.message, code: error.code });
-      }
-    }
+    return response.json(client);
   }
 }
