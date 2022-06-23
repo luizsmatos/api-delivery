e993489d9e51367d243e6d99f408c95268d5cdee
Luiz Gustavo <gusstavopdm@gmail.com>
2022-06-23 08:35:09 -0300

    feat(routes): adicionando rota client/deliveries

--- src/routes/routes.client.ts
+++ src/routes/routes.client.ts
@@ -1,13 +1,22 @@
 import { Router } from 'express';
+
+import { FindAllDeliveriesController } from './../modules/clients/useCases/findAllDeliveries/FindAllDeliveriesController';
 import { AuthenticateClientController } from '../modules/account/authenticateClient/AuthenticateClientController';
 import { CreateClientController } from '../modules/clients/useCases/createClient/CreateClientController';
+import { ensureAuthenticateClient } from '../middlewares/ensureAuthenticateClient';
 
 const routesClient = Router();
 
 const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
+const findAllDeliveriesController = new FindAllDeliveriesController();
 
 routesClient.post('/', createClientController.handle);
 routesClient.post('/authenticate', authenticateClientController.handle);
+routesClient.get(
+  '/deliveries',
+  ensureAuthenticateClient,
+  findAllDeliveriesController.handle,
+);
 
 export { routesClient };
