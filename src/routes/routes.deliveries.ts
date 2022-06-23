134a1746bdd042cfda48619d41ac0c06c885b81e
Luiz Gustavo <gusstavopdm@gmail.com>
2022-06-23 09:11:32 -0300

    feat(routes): adicionando rota deliveries/updateStatus

--- src/routes/routes.deliveries.ts
+++ src/routes/routes.deliveries.ts
@@ -1,21 +1,29 @@
 import { Router } from 'express';
 
-import { UpdateDeliverymanController } from './../modules/deliveries/useCases/updateDeliveryman/UpdateDeliverymanController';
-import { CreateDeliveryController } from '../modules/deliveries/useCases/createDelivery/CreateDeliveryController';
-import { FindAllAvailableController } from '../modules/deliveries/useCases/findAllAvailable/FindAllAvailableController';
 import { ensureAuthenticateClient } from '../middlewares/ensureAuthenticateClient';
 import { ensureAuthenticateDeliveryman } from '../middlewares/ensureAuthenticateDeliveryman';
+import { CreateDeliveryController } from '../modules/deliveries/useCases/createDelivery/CreateDeliveryController';
+import { FindAllAvailableController } from '../modules/deliveries/useCases/findAllAvailable/FindAllAvailableController';
+import { UpdateDeliverymanController } from './../modules/deliveries/useCases/updateDeliveryman/UpdateDeliverymanController';
+import { UpdateStatusDeliveryController } from '../modules/deliveries/useCases/updateStatusDelivery/UpdateStatusDeliveryController';
 
 const routesDeliveries = Router();
 
 const createDeliveryController = new CreateDeliveryController();
 const findAllAvailableController = new FindAllAvailableController();
 const updateDeliverymanController = new UpdateDeliverymanController();
+const updateStatusDeliveryController = new UpdateStatusDeliveryController();
 
 routesDeliveries.put(
   '/updateDeliveryman/:id',
   ensureAuthenticateDeliveryman,
   updateDeliverymanController.handle,
+);
+
+routesDeliveries.put(
+  '/updateStatus/:id',
+  ensureAuthenticateDeliveryman,
+  updateStatusDeliveryController.handle,
 );
 
 routesDeliveries.post(
